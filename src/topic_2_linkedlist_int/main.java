01e46d4fc73b626303dfbfd1ac626f653dfc0194
Jhandie <sumihayjhandie@gmail.com>
2024-10-03 08:24:50 -0700

    import scanner

--- src/topic_2_linkedlist_int/main.java
+++ src/topic_2_linkedlist_int/main.java
@@ -1,15 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package topic_2_linkedlist_int;
 
-/**
- *
- * @author Corazon
- */
+import java.util.Scanner;
+
 public class main {
-    
+
+    public static void main(String[] args) {
+        Linkedlist list = new Linkedlist();
+
+        //adding elements to the list
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        System.out.println("Current Linked List:");
+        list.printList();
+
+        //Delete
+        System.out.println();
+        System.out.println("Deleting 40 from the list...");
+        System.out.println("Current Linked List:");
+        list.deleteByValue(20);
+        list.printList();
+
+        //Move/Swap Pointer
+        System.out.println();
+        System.out.println("Movingn/Swapping node from index 1 to index 0");
+        list.moveNodePointer(0, 1);
+        list.printList();
+    }
 }
